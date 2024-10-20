e173ba9d8ceb6d7c45540007916d981a1bb9661a
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-10-20 09:39:34 +0500

    updated with App style class

--- tickspot/lib/home_view.dart
+++ tickspot/lib/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tickspot/Controller.dart';
+import 'package:tickspot/theme/App-styles.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -11,11 +12,41 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.transparent,
-      body: Center(
-        child: Text("data"),
-      ),
-    );
+    return Scaffold(
+        backgroundColor: Colors.transparent,
+        body: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Good Morning",
+                            style: AppStyles.headlineStyle3,
+                          ),
+                          Text(
+                            "Book TIckets",
+                            style: AppStyles.headlineStyle1,
+                          )
+                        ],
+                      ),
+                      Container(
+                        height: 70,
+                        width: 70,
+                        color: Colors.teal,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
